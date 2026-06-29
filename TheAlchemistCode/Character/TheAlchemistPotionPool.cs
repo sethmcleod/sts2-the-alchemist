8a8a3913565f4c378c07756b469a73a680e706f4
Seth McLeod <sethmcleod@pm.me>
2026-06-28 22:24:43 -0400

    feat: initial commit

---
+++ TheAlchemistCode/Character/TheAlchemistPotionPool.cs
@@ -0,0 +1,14 @@
+﻿using BaseLib.Abstracts;
+using TheAlchemist.TheAlchemistCode.Extensions;
+using Godot;
+
+namespace TheAlchemist.TheAlchemistCode.Character;
+
+public class TheAlchemistPotionPool : CustomPotionPoolModel
+{
+    public override Color LabOutlineColor => TheAlchemist.Color;
+
+
+    public override string BigEnergyIconPath => "charui/big_energy.png".ImagePath();
+    public override string TextEnergyIconPath => "charui/text_energy.png".ImagePath();
+}
---
+++ TheAlchemistCode/Potions/TheAlchemistPotion.cs
@@ -0,0 +1,8 @@
+﻿using BaseLib.Abstracts;
+using BaseLib.Utils;
+using TheAlchemist.TheAlchemistCode.Character;
+
+namespace TheAlchemist.TheAlchemistCode.Potions;
+
+[Pool(typeof(TheAlchemistPotionPool))]
+public abstract class TheAlchemistPotion : CustomPotionModel;
