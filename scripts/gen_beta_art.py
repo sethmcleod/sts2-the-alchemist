9a7967b4074e6a4f0d4f8a52d197d71c30a77eb1
Seth McLeod <sethmcleod@pm.me>
2026-08-13 16:25:15 -0400

    feat: update some beta art

--- scripts/gen_beta_art.py
+++ scripts/gen_beta_art.py
@@ -81,35 +81,26 @@ BAYER = [
 # Direction encodes card type: Attack TR, Skill BR, Power R or L, token vertical (U or D)
 COLORS = {
     "Tempered": ("#1f1723", "#5f7fa8", "BR", "Unique"),
-    # Poison powers: the self-poison payoffs
     "Callus": ("#071a02", "#c2703a", "R", "Poison powers: amplifiers"),
     "Rot": ("#071a02", "#6b8f2a", "L", "Poison powers: retaliation"),
     "Swamp Gas": ("#4e8701", "#1c5f7a", "BR", "Poison skills"),
     "Gulp": ("#4e8701", "#8a3f2f", "BR", "Poison skills"),
     "Recoil": ("#00212a", "#3fb0a0", "TR", "Poison payoffs"),
-    # Antitoxin
     "Purge": ("#2f0170", "#6f9ad8", "BR", "Antitoxin"),
     "Immunize": ("#2f0170", "#c46fd8", "R", "Antitoxin"),
     "Pass It On": ("#2f0170", "#d84a4a", "L", "Antitoxin"),
-    # Infuse
     "Deep Breath": ("#792595", "#9ad8b8", "BR", "Infuse: cantrips"),
     "Kickback": ("#4a1a66", "#8fd84a", "R", "Infuse: heavy"),
     "Backfire": ("#0b2a4a", "#5fd0e8", "TR", "Unique"),
     "Croak": ("#5f011f", "#b3bd2a", "TR", "Poison attacks"),
-    # Cauterize (one-off)
     "Overdose": ("#5f011f", "#c18c0f", "TR", "Unique"),
     "Air It Out": ("#3d0a02", "#e0d6c2", "TR", "Unique"),
-    # Puncture (one-off)
     "Puncture": ("#0d1f3d", "#3d9ae8", "TR", "Unique"),
-    # Lash Out (one-off)
     "Lash Out": ("#5f011f", "#77828c", "TR", "Unique"),
-    # Potion and buff skills
     "Congeal": ("#1d3f0e", "#0e7c7b", "BR", "Potion and buff skills"),
     "Transmute": ("#5f011f", "#e3b84a", "BR", "Potion and buff skills"),
     "Warded": ("#45293f", "#c98fd6", "R", "Unique"),
-    # Antitoxin: the purple counterpart to the poison reds
     "One For Me": ("#2f0170", "#8f6fd8", "TR", "Antitoxin"),
-    # Poison attacks
     "Heavy Dose": ("#650101", "#c46a12", "TR", "Poison attacks"),
     "Flare Up": ("#650101", "#d9b81e", "TR", "Poison attacks"),
     "Spatter": ("#650101", "#7ae801", "TR", "Poison attacks"),
@@ -119,114 +110,69 @@ COLORS = {
     "Next Up": ("#2f0170", "#c0a24a", "TR", "Unique"),
     "Cheap Cover": ("#1f1723", "#7f9aa8", "BR", "Exhaust pile: skills"),
     "Fallout": ("#650101", "#4a7a1c", "TR", "Poison attacks"),
-    # Unstable Compound (one-off): the name kept its own violet-into-toxic-green
     "Slow Burn": ("#2f0170", "#63c11c", "BR", "Unique"),
-    # Poison and Infuse skills
     "Corrode": ("#2f0170", "#287912", "BR", "Poison and Infuse skills"),
     "Trade Up": ("#2f0170", "#2aa88a", "BR", "Poison and Infuse skills"),
-    # Heavy attacks
     "Draining Strike": ("#2f0170", "#b3341f", "TR", "Heavy attacks"),
-    # Light attacks
     "Quicklime": ("#1c0f45", "#e8e4d2", "TR", "Light attacks"),
-    # Ferment: attacks
     "Patient Strike": ("#4a0e2e", "#c78a3b", "TR", "Ferment: attacks"),
     "Rolling Boil": ("#4a0e2e", "#e85a1f", "TR", "Ferment: attacks"),
-    # Ferment: skills and powers
     "All At Once": ("#6b2444", "#c2601c", "TR", "Unique"),
     "Puff Up": ("#6b2444", "#d9a86a", "BR", "Ferment: skills and powers"),
     "Vintage": ("#6b2444", "#df9723", "BR", "Ferment: skills and powers"),
-    # Infuse: cantrips
     "Enrich": ("#792595", "#b8d94f", "BR", "Infuse: cantrips"),
     "Salve": ("#792595", "#6fd8e0", "BR", "Infuse: cantrips"),
-    # Infuse: heavy
     "Masterwork": ("#4a1a66", "#c9a227", "BR", "Infuse: heavy"),
     "Refine": ("#4a1a66", "#257bc3", "BR", "Infuse: heavy"),
-    # Froth (one-off: sea-foam over deep blue)
     "Froth": ("#0e3550", "#8fe0c8", "TR", "Unique"),
-    # Siphon (one-off)
     "Siphon": ("#241a5e", "#2aa89b", "TR", "Unique"),
-    # Enchanted payoffs: debuff attacks
-    "Echo Strike": ("#5b3fc4", "#d84a6f", "TR", "Enchanted payoffs: debuff attacks"),
+    "Echo Strike": ("#5b3fc4", "#4a9fd8", "TR", "Enchanted payoffs: debuff attacks"),
     "Needle Point": ("#5b3fc4", "#aeb4bc", "TR", "Enchanted payoffs: debuff attacks"),
-    "Deep Cut": ("#5b3fc4", "#d94f6a", "TR", "Enchanted payoffs: debuff attacks"),
-    # Enchanted payoffs: Antitoxin
+    "Deep Cut": ("#5b3fc4", "#4ad89b", "TR", "Enchanted payoffs: debuff attacks"),
     "Harden": ("#5b3fc4", "#d9c89a", "R", "Enchanted payoffs: Antitoxin"),
     "Vitrify": ("#5b3fc4", "#48495f", "BR", "Enchanted payoffs: Antitoxin"),
-    # Golden Touch (one-off)
     "Golden Touch": ("#5b3fc4", "#ffc832", "L", "Unique"),
-    # Potion: attacks
-    # Potion: engines
-    # Azoth (one-off)
     "Fast Acting": ("#1f1723", "#8a5f9e", "TR", "Unique"),
-    # Fumigate (one-off)
     "Smoke Out": ("#1f1723", "#a3a83a", "TR", "Unique"),
-    # Exhaust pile: skills
     "Poultice": ("#1f1723", "#4f9e4a", "BR", "Exhaust pile: skills"),
     "Rummage": ("#1f1723", "#be401b", "BR", "Exhaust pile: skills"),
-    # Poison as a resource to spend
     "Overflow": ("#00212a", "#10aba5", "BR", "Poison payoffs"),
     "Blood Rush": ("#14424e", "#b8e86a", "BR", "Poison payoffs"),
-    # Poison skills
     "Fester": ("#4e8701", "#0139b2", "BR", "Poison skills"),
     "Sweat It Out": ("#4e8701", "#013161", "BR", "Poison skills"),
     "Waste Not": ("#4e8701", "#2f4f8f", "BR", "Poison skills"),
     "Taint": ("#4e8701", "#7a2f6b", "BR", "Poison skills"),
-    # Poison powers: amplifiers
     "Heavy Hand": ("#071a02", "#8fc72a", "R", "Poison powers: amplifiers"),
     "Weak Spot": ("#071a02", "#5aea23", "L", "Poison powers: amplifiers"),
-    # Poison powers: retaliation
     "Contagion": ("#071a02", "#c9b428", "R", "Poison powers: retaliation"),
     "Grudge": ("#071a02", "#5fbf8a", "R", "Poison powers: retaliation"),
     "Share The Pain": ("#071a02", "#7ad14a", "L", "Poison powers: retaliation"),
     "Toxin Skin": ("#071a02", "#af8423", "L", "Poison powers: retaliation"),
-    # Poison plus Regen
     "Mercurial Form": ("#0e6b54", "#8aa312", "R", "Poison plus Regen"),
     "Pays Off": ("#14805f", "#a6c214", "L", "Poison plus Regen"),
-    # Burst tempo
     "White Heat": ("#7a1c02", "#ffdf8a", "BR", "Burst tempo"),
     "Bitter Draught": ("#7a1c02", "#d98cc8", "BR", "Burst tempo"),
-    # Crafting: transform
     "Melt Down": ("#1b2a44", "#0f9b82", "BR", "Crafting: transform"),
     "Boil Down": ("#1b2a44", "#0c7f56", "BR", "Crafting: transform"),
-    # Crafting: discovery
     "Decant": ("#1b2a44", "#8a63f5", "TR", "Crafting: discovery"),
     "Eureka": ("#1b2a44", "#f2c94e", "BR", "Crafting: discovery"),
-    # Crafting: refinement
     "Hone": ("#3a1220", "#ff5f3d", "BR", "Crafting: refinement"),
     "Winnow": ("#3a1220", "#f0435e", "BR", "Crafting: refinement"),
-    # Distillate (one-off token)
     "Distillate": ("#1b2f2c", "#6fe0b4", "D", "Unique"),
-    # Multiplayer: gifts
     "Bestow": ("#2563c4", "#b48ff0", "BR", "Multiplayer: gifts"),
     "Effervesce": ("#2563c4", "#d8e8f5", "BR", "Multiplayer: gifts"),
-    # Reflux (one-off)
     "Reflux": ("#2563c4", "#178f5f", "BR", "Unique"),
-    # Suffuse (one-off)
-    # Desperation: powers
     "Fresh Coat": ("#240147", "#5fa8d3", "R", "Unique"),
     "Resolve": ("#240147", "#e04a3c", "R", "Desperation: powers"),
-    # Ichor (one-off)
-    # Inoculate (one-off)
     "Sleep On It": ("#0e2a45", "#7fd4c1", "BR", "Unique"),
     "Inoculate": ("#12384f", "#8fd0e8", "BR", "Unique"),
-    # Strike (Basic)
     "Strike": ("#180209", "#942d2d", "TR", "Unique"),
-    # Defend (Basic)
     "Defend": ("#0f2a43", "#2b6ea8", "BR", "Unique"),
-    # Spike (Basic): the starter dose, anchored on the Poison-attack red
-    "Jab": ("#2b0410", "#46621f", "TR", "Unique"),
-    # Flask (Basic): the starter brew, anchored on the potion teal
+    "Jab": ("#2d0a45", "#5b8028", "TR", "Unique"),
     "Antidote": ("#045062", "#d8b23a", "BR", "Unique"),
-    # Nigredo (token)
-    # Albedo (token)
-    # Rubedo (token)
-    # Elixir (Ancient)
     "Elixir": ("#7638ff", "#22ff88", "BL", "Unique"),
-    # Aureate (Ancient)
     "Wormwood": ("#4a0e2e", "#6ba32c", "TR", "Unique"),
-    # Golden Fruit (Quest)
     "Golden Fruit": ("#c58037", "#ffef2d", "TR", "Unique"),
-    # Unripe Fruit (Quest)
     "Unripe Fruit": ("#26a82b", "#ffef2d", "TR", "Unique"),
 }
 
